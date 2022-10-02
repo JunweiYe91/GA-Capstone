5f93973fcbcf3676145cdfa9b95b6f3f0fdca551
JunweiYe91 <ye_junwei@hotmail.com>
2022-10-03 02:15:28 +0800

    update of documents

--- Home.py
+++ Home.py
@@ -19,27 +19,14 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # Set Page Title
-st.set_page_config(page_title="Home", layout="wide")
+st.set_page_config(page_title="Home Page", layout="wide")
 
 # Set the title
 title = "Welcome to KEYCARDS!"
 st.title(title)
 
 # sidebar contents
-st.sidebar.markdown(
-        """
-        <style>
-            [data-testid="stSidebarNav"] {
-                background-image: url(https://lh3.googleusercontent.com/eRZ62FDqGZphgSBxSlibMF-6aNpRCIVslttJJLlsVbyECA6wVcT-KEtZOQ-FfY0VVzIFznTvTXK3eHYd7gjtfXQ1OZnd1z1pM81BiDjit_I7tqp93f7qzASs6I8tmMKkgtVhqjgktrk=w2400);
-                background-repeat: no-repeat;
-                padding-top: 2px;
-                background-position: 20px 30px;
-            }
-        </style>
-        """,
-        unsafe_allow_html=True,
-)
-
+st.sidebar.image(Image.open('image/logo/logo.png'))
 st.sidebar.write("For Careers/Business Opportunity, please contact me at:")
 st.sidebar.write("**Email:** ye_junwei@hotmail.com")
 st.sidebar.write("**Github:** https://github.com/JunweiYe91/GA-Projects")
